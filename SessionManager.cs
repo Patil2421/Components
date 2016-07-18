92a0759568d5b132758d2f047e33b6b0f0eabbd8
Patil2421 <amitpatil2421@gmail.com>
2016-07-18 12:29:08 +0530

    SessionManager-Xamarin Forms
    Add this file to your common folder in Shared Project

---
+++ SessionManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Champagn
+{
+	public static class SessionManager
+	{
+		public static string AccessToken
+		{
+			get
+			{
+				if (Application.Current.Properties.ContainsKey ("AccessToken"))
+					return string.IsNullOrEmpty (Application.Current.Properties ["AccessToken"].ToString ())
+						? string.Empty : Convert.ToString (Application.Current.Properties ["AccessToken"]);
+				else
+					return string.Empty;
+			}
+			set
+			{
+				Application.Current.Properties ["AccessToken"] = value;
+			}
+		}
+
+		public static string EmailAddress
+		{
+			get
+			{
+				if (Application.Current.Properties.ContainsKey ("EmailAddress"))
+					return string.IsNullOrEmpty (Application.Current.Properties ["EmailAddress"].ToString ()) ? string.Empty : Convert.ToString (Application.Current.Properties ["EmailAddress"]);
+				else
+					return string.Empty;
+			}
+			set
+			{
+				Application.Current.Properties ["EmailAddress"] = value;
+			}
+		}
+
+		public static Int32 UserId
+		{
+			get
+			{
+				return Application.Current.Properties.ContainsKey ("UserId") ? Convert.ToInt32 (Application.Current.Properties ["UserId"]) : 0;
+			}
+			set
+			{
+				Application.Current.Properties ["UserId"] = value;
+			}
+		}	
+
+		public static string UserName
+		{
+			get
+			{
+				if (Application.Current.Properties.ContainsKey ("UserName"))
+					return string.IsNullOrEmpty (Application.Current.Properties ["UserName"].ToString ()) ? string.Empty : Convert.ToString (Application.Current.Properties ["UserName"]);
+				else
+					return string.Empty;
+			}
+			set
+			{
+				Application.Current.Properties ["UserName"] = value;
+			}
+		}
+
+    }
+}
+
