6d03d78166c7c934f34ba6a98d5e5228446c8bf3
Patil2421 <amitpatil2421@gmail.com>
2016-08-12 13:20:06 +0530

    Update Implementation.cs

--- Implementation.cs
+++ Implementation.cs
@@ -1,51 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
-
 using Xamarin.Forms;
 
-namespace Champagn
+namespace YourProject
 {
-	public partial class Mark2 : ContentPage
+	public Mark2 ()
 	{
-		public Mark2 ()
-		{
-			InitializeComponent ();
-
-			byte[] ScreenShotdata = await DependencyService.Get<IScreenshotManager> ().CaptureAsync ();
-
-			GestureFrame gi = new GestureFrame
-			{
-				HorizontalOptions = LayoutOptions.FillAndExpand,
-				VerticalOptions = LayoutOptions.FillAndExpand,
-				BackgroundColor = Color.FromHex("bf3122"),
-			};
-
-			gi.SwipeDown += (s, e) =>
-			{
-				DisplayAlert("Gesture Info", "Swipe Down Detected", "OK");
-			};
-
-			gi.SwipeTop += (s, e) =>
-			{
-				DisplayAlert("Gesture Info", "Swipe Top Detected", "OK");
-
-			};
-
-			gi.SwipeLeft += (s, e) =>
-			{
-				DisplayAlert("Gesture Info", "Swipe Left Detected", "OK");
-
-			};
-
-			gi.SwipeRight += (s, e) =>
-			{
-				DisplayAlert("Gesture Info", "Swipe Right Detected", "OK");
-
-			};
-
-			this.Content = gi;
-		}
+		InitializeComponent ();
+		//Gets the Screenshot Image in byte[]
+		byte[] ScreenShotdata = await DependencyService.Get<IScreenshotManager> ().CaptureAsync ();
+		
 	}
 }
 
