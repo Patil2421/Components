90312cb6103ff83a25aabe036101ff32008f823b
Patil2421 <amitpatil2421@gmail.com>
2016-08-12 13:18:09 +0530

    Update IPlatformPreferences.cs

--- Interfaces_Shared/IPlatformPreferences.cs
+++ Interfaces_Shared/IPlatformPreferences.cs
@@ -1,21 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Champagn
+namespace YourProject
 {
-	public interface IPlatformPreferences
-	{
-		Collection<string> getValues();
-		void saveValues(Collection<string> values);
-		void clearValues();
-	}
-
 	public interface IScreenshotManager
 	{
-		byte[] CaptureAsync(int height,int width);
+		byte[] CaptureAsync();
 	}
 }
